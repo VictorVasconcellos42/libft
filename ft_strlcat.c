b4b3db2c945096170fbeaeac9997a01f06354dbe
vde-vasc <vde-vasc@student.42.rio>
2022-05-17 21:32:54 -0300

    Hotfix function & putting in norm standard

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -17,36 +17,41 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	len_dst;
 	size_t	i;
-	size_t	len_src;
 
-	len_dst = ft_strlen(dst);
-	len_src = ft_strlen(src);
-	if (dstsize == 0)
-		return (len_src);
-	else if (len_dst >= dstsize)
-		return (len_src + dstsize);
+	len_dst = 0;
+	while (dst[len_dst] && len_dst < dstsize)
+		len_dst++;
+	if (len_dst == dstsize)
+		return (ft_strlen(src) + len_dst);
 	i = 0;
-	while (i < (dstsize - len_dst))
+	while (src[i] && i < (dstsize - len_dst) - 1)
 	{
 		dst[len_dst + i] = src[i];
 		i++;
 	}
-	dst[len_dst + i - 1] = '\0';
-	return (len_dst + len_src);
+	dst[len_dst + i] = '\0';
+	return (len_dst + ft_strlen(src));
 }
 
-/* #include <stdio.h>
+/*#include <stdio.h>
 #include <string.h>
 
 int	main(void)
 
 {
-	char dst[10] = "a";
-	char src[] = "bcd";
-	char dst2[10] = "a";
-	char src2[] = "bcd";
+	char *dst;
+	char src[] = "lorem";
+	char *dst2;
+	char src2[] = "lorem";
 	size_t	ret;
 	size_t	ret2;
+
+	dst = malloc(15 * sizeof(char *));
+	dst2 = malloc(15 * sizeof(char *));
+	memset(dst, 'r', 6);
+	memset(dst2, 'r', 6);
+	memset(dst, '\', 15);
+	memset(dst2, '\0', 15);
 	ret = strlcat(dst, src, sizeof(dst));
 	ret2 = ft_strlcat(dst2, src2, sizeof(dst));
 	if (ret == ret2)
@@ -56,4 +61,4 @@ int	main(void)
 	printf("original = %lu\nminha = %lu\n", ret, ret2);
 	puts(dst);
 	puts(dst2);
-} */
+}*/
