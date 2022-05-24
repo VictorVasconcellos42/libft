9b2e560112d900eb717ff8bf7a916f7d3cd96101
vde-vasc <vde-vasc@student.42.rio>
2022-05-24 17:58:49 -0300

    update libft.h with split function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 18:23:30 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/05/18 10:35:34 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/05/24 10:32:20 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,4 +45,5 @@ char	*ft_strjoin(char const *s1, char const *s2);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 #endif
