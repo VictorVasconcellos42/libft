ca6beff70d9e69275fa6d65c2981904147ae9c03
vde-vasc <vde-vasc@student.42.rio>
2022-05-30 19:31:40 -0300

    Finally, split 100%git add ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,36 +6,50 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 19:47:40 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/05/24 17:58:12 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/05/28 23:06:45 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	*freedom(char	**array)
+
+{
+	int	i;
+
+	i = 0;
+	while (array[i++])
+		free(array[i]);
+	free(array[i]);
+	free(array);
+	return (NULL);
+}
+
 static char	**split_string(char const *string, char c, char **array)
 
 {
 	size_t	i;
-	int		len_words;
-	int		position;
+	int		len;
+	int		point;
 
-	position = 0;
-	len_words = 0;
+	point = 0;
+	len = 0;
 	i = 0;
-	if (string[i] == c)
+	while (string[i] == c)
 		i++;
 	while (i <= ft_strlen(string))
 	{
-		if ((string[i] == c && len_words != 0) || \
-			(string[i] == '\0' && len_words != 0))
+		if ((string[i] == c && len != 0) || \
+		(string[i] == '\0' && len != 0))
 		{
-			array[position] = ft_calloc(len_words + 1, sizeof(char *));
-			ft_strlcpy(array[position], &string[i - len_words], len_words + 1);
-			position++;
-			len_words = 0;
+			array[point] = ft_calloc(len + 1, sizeof(char));
+			if (!(array[point]))
+				freedom(array);
+			ft_strlcpy(array[point++], &string[i - len], len + 1);
+			len = 0;
 		}
 		else if (string[i] != c)
-			len_words++;
+			len++;
 		i++;
 	}
 	return (array);
@@ -51,6 +65,8 @@ static int	how_many_words(char const *string, char c, size_t string_len)
 	i = 0;
 	count_words = 0;
 	len_words = 0;
+	if (string[i] == c)
+		i++;
 	while (i < string_len)
 	{
 		if (string[i] == c && len_words != 0)
@@ -62,7 +78,7 @@ static int	how_many_words(char const *string, char c, size_t string_len)
 			len_words++;
 		i++;
 	}
-	if (len_words != 0 && count_words > 0)
+	if (len_words != 0 && count_words >= 0)
 		count_words++;
 	return (count_words);
 }
@@ -77,44 +93,11 @@ char	**ft_split(char const *string, char c)
 
 	if (!(string))
 		return (NULL);
-	if (c == '\0')
-	{
-		array_of_strings = malloc(1 * sizeof(*array_of_strings));
-		array_of_strings[0] = NULL;
-		return (array_of_strings);
-	}
 	string_len = ft_strlen(string);
 	count_words = how_many_words(string, c, string_len);
-	if (count_words == 0 && c == '\0')
-		return (NULL);
 	array_words = ft_calloc(count_words + 1, sizeof(char *));
 	if (!(array_words))
-		return (NULL);
+		freedom(array_words);
 	array_of_strings = split_string(string, c, array_words);
 	return (array_of_strings);
 }
-
-/*#include <stdio.h>
-
-int	main(void)
-
-{
-	char	*source = "minha,casa,minha,vida";
-	char	c = ',';
-	char	**array;
-
-	array = ft_split(source, c);
-	if	(!(array))
-	{
-		printf("KO TOTAL!Z\n");
-		return 0;
-	}
-	for (size_t i = 0;i < 10; i++)
-	{
-		printf("%s\n", array[i]);
-	}
-	int i = 0;
-	while(array[i])
-		free(array[i++]);
-	return 0;
-}*/
