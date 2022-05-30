e60cd45bcd950184602a508c37d62d397b5b1057
vde-vasc <vde-vasc@student.42.rio>
2022-05-30 19:35:51 -0300

    Protected trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 10:08:05 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/05/24 10:43:23 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/05/28 18:15:42 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	size_end;
 	char	*string;
 
+	if (!(s1) || !(set))
+		return (NULL);
 	size_end = ft_strlen(s1) - 1;
 	size_start = 0;
 	while (ft_strchr(set, s1[size_start]))
