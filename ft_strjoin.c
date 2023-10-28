f92660fe91f73bb048a62e38b9572398131cdc71
Victor Vasconcellos <victorvasconcellos42@gmail.com>
2023-10-28 16:21:47 -0300

    Feat: new strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,28 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 01:36:13 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/05/18 02:14:01 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/10/28 16:16:34 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
-
 {
-	char	*string;
-	size_t	len_s1;
-	size_t	len_s2;
+	char	*newString;
+	int		i;
+	int		j;
+	size_t	size;
 
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	string = malloc(len_s1 + len_s2 + 1);
-	if (!(string))
+	if (!s1 || !s2)
 		return (NULL);
-	ft_strlcpy(string, s1, len_s1 + 1);
-	ft_strlcpy(string + len_s1, s2, len_s2 + 1);
-	return (string);
+	size = (ft_strlen(s1) + ft_strlen(s2)) + 1;
+	newString = malloc(sizeof(char) * size);
+	if (!newString)
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		newString[i] = s1[i];
+	j = -1;
+	while (s2[++j])
+		newString[i++] = s2[j];
+	newString[i] = '\0';
+	return (newString);
 }
