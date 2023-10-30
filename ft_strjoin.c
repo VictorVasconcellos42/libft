aaaa458add0b3e4c000f68cb4782b2fb2635e157
Victor Vasconcellos <victorvasconcellos42@gmail.com>
2023-10-29 22:15:56 -0300

    Fix: norm standard strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 01:36:13 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/10/28 16:16:34 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/10/29 19:25:55 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*newString;
+	char	*new;
 	int		i;
 	int		j;
 	size_t	size;
@@ -22,15 +22,15 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (!s1 || !s2)
 		return (NULL);
 	size = (ft_strlen(s1) + ft_strlen(s2)) + 1;
-	newString = malloc(sizeof(char) * size);
-	if (!newString)
+	new = malloc(sizeof(char) * size);
+	if (!new)
 		return (NULL);
 	i = -1;
 	while (s1[++i])
-		newString[i] = s1[i];
+		new[i] = s1[i];
 	j = -1;
 	while (s2[++j])
-		newString[i++] = s2[j];
-	newString[i] = '\0';
-	return (newString);
+		new[i++] = s2[j];
+	new[i] = '\0';
+	return (new);
 }
